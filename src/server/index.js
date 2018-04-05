c43681e716022a7bfbc96cb57fff474849184b8a
Berkeley True <berkeleytrue@users.noreply.github.com>
2018-04-04 18:08:59 -0700

    feat(server): Render initial template

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,17 @@ log.enabled = true;
 const app = express();
 app.set('port', process.env.PORT);
 
-app.get('/', (req, res) => res.send('foo'));
+const renderHtml = ({ message }) => `
+<html>
+<head>
+  <title>wndlr</title>
+</head>
+<body>
+  <h1>${message}</h1>
+</body>
+`;
+
+app.get('/', (req, res) => res.send(renderHtml({ message: 'hello wndlr' })));
 
 app.listen(app.get('port'), () => {
   log(`server started on port ${app.get('port')}`);
