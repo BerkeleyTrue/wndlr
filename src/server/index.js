dacd634e07fc9596491fb6b796f4daf502917f10
Berkeley True <berkeleytrue@users.noreply.github.com>
2018-04-04 17:39:12 -0700

    feat(server): Serving app

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,17 @@
+import 'dotenv/config';
 import express from 'express';
 import createDebugger from 'debug';
 
 const log = createDebugger('wndlr:server');
+const isDev = process.env.NODE_ENV !== 'production';
+log.enabled = true;
 
 const app = express();
+app.set('port', process.env.PORT);
 
 app.get('/', (req, res) => res.send('foo'));
 
 app.listen(app.get('port'), () => {
-  log('server started');
+  log(`server started on port ${app.get('port')}`);
+  log(`server is in ${isDev ? 'dev' : 'prod'} mode`);
 });
