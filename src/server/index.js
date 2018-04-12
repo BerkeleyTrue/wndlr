2d6e49b45e7a96a0dfb05009dd31e7e3e59830cd
Berkeley True <berkeleytrue@users.noreply.github.com>
2018-04-11 19:42:08 -0700

    feat(server): Add webpack dev middleware to server bundle

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 import createDebugger from 'debug';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import config from '../../webpack.config.js';
 
 const log = createDebugger('wndlr:server');
-const isDev = process.env.NODE_ENV !== 'production';
 log.enabled = true;
+const isDev = process.env.NODE_ENV !== 'production';
+const compiler = webpack(config());
 
 const app = express();
 app.set('port', process.env.PORT);
@@ -22,6 +26,7 @@ const renderHtml = () => `
 `;
 
 app.use(morgan('dev'));
+app.use(webpackDevMiddleware(compiler, { heartbeat: 10 * 1000 }));
 app.use(express.static('dist'));
 app.get('/', (req, res) => res.send(renderHtml({ message: 'hello wndlr' })));
 
