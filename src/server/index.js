7f61c98dc6663573438e2961501f64db1b16f2e0
Berkeley True <berkeleytrue@users.noreply.github.com>
2018-04-15 17:40:28 -0700

    fix(server): Add start func for server

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import _ from 'lodash';
 import express from 'express';
 import morgan from 'morgan';
 import createDebugger from 'debug';
@@ -19,8 +20,26 @@ devServer(app);
 app.use(express.static('dist'));
 renderReact(app);
 
-app.listen(app.get('port'), () => {
-  app.emit('started');
-  log(`server started on port ${app.get('port')}`);
-  log(`server is in ${isDev ? 'dev' : 'prod'} mode`);
+app.start = _.once(() => {
+  const server = app.listen(app.get('port'), () => {
+    app.emit('started');
+    log(`server started on port ${app.get('port')}`);
+    log(`server is in ${isDev ? 'dev' : 'prod'} mode`);
+  });
+
+  // on signal to kill, close server
+  process.on('SIGINT', () => {
+    log('Shutting down server');
+    server.close(() => {
+      log('Server is closed');
+    });
+    // TODO: Disconnect db
+  });
 });
+
+module.exports = app;
+
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
