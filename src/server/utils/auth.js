e8cf42edd76da12e8724573a6eaf5a3f23b55fdf
Berkeley True <berkeleytrue@users.noreply.github.com>
2019-04-23 21:22:13 -0700

    fix(server/utils/auth): give token gen arity of 0

--- src/server/utils/auth.js
+++ src/server/utils/auth.js
@@ -1,4 +1,8 @@
+import * as R from 'ramda';
 import uid from 'uid2';
 import { bindNodeCallback } from 'rxjs';
 
-export const generateVerificationToken = bindNodeCallback(uid.bind(null, 64));
+export const generateVerificationToken = R.nAry(
+  0,
+  bindNodeCallback(uid.bind(null, 64)),
+);
